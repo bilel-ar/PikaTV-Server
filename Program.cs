da3b223c5b76195ddf69dd09a81ace707a5a2c04
Bilel AR <bilel4186@gmail.com>
2026-05-20 14:07:30 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -44,7 +44,7 @@ app.MapGet("/health", () => Results.Ok(new { status = "healthy" }));
 // قاعدة بيانات الأكواد
 var userSubscriptions = new Dictionary<string, string>
 {
-    { "1311", "http://45.11.57.152:8000/get.php?username=DkedJpyzjY&password=59K97DVFBL&type=m3u_plus" },
+    { "1311", "http://206.109.57.241/get.php?username=09988713622&password=13939496650&type=m3u_plus" },
     { "0202", "http://casa04.com:80/get.php?username=salwa365121&password=cx3oarntlz&type=m3u_plus" }
 };
 
